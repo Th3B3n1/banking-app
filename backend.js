541d60fd0c7c2963b88faba14fab86f867db8019
Th3B3n1 <othersc9bst@gmail.com>
2024-05-15 14:21:25 +0200

    login done

--- backend.js
+++ backend.js
@@ -23,7 +23,7 @@ const db = mysql.createPool({
 const jsonParser = bodyParser.json();
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const saltRounds = 10;
-const tokenExpiry = 5;
+const tokenExpiryInDays = 5;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
@@ -41,66 +41,91 @@ app.post("/login", jsonParser, async (req, res) =>
         {
             try
             {
-                let query = await db.query("SELECT * FROM tokens WHERE token = ?", req.body.token);
-                console.log(query[0].length)
-                if (query[0].length > 0)
+                let tokenQuery = await db.query("SELECT userid, date, token FROM tokens WHERE token = ?", req.body.token);
+                if (tokenQuery[0].length > 0)
                 {
-                    if (isExpiredBasedOnCurrentDateTime(query[0][0].date, tokenExpiry))
+                    if (isTokenValidBasedOnCurrentDateTime(tokenQuery[0][0].date, tokenExpiryInDays))
                     {
-                        console.log("yr")
+                        let usersQuery = await db.query("SELECT email, fullname, balance FROM users WHERE id = ?", tokenQuery[0][0].userid)
+                        res.status(200);
+                        res.send(usersQuery[0][0]).end();
                     }
                     else
                     {
-                        console.log("yf")
+                        res.send({"message": "INFO: The token expired. Please log in again."});
                     }
                 }
                 else
                 {
                     res.status(404);
-                    res.send({"message": "ERROR: Token not found"}).end();
+                    res.send({"message": "ERROR: Token not found. Please log in again."}).end();
                 }
             }
             catch (error)
             {
                 res.status(503);
-                res.send({"message": "ERROR: Internal server error (Error message: " + error + ")"}).end();
+                res.send({"message": "ERROR: " + error}).end();
             }
         }
-        else if (req.body.username != undefined && req.body.password != undefined)
+        else if (req.body.email != undefined && req.body.password != undefined)
         {
-            if (req.body.username.length > 0 && req.body.password > 0)
+            if (req.body.email.length > 0 && req.body.password > 0)
             {
                 try
                 {
-                    let query = await db.query("SELECT * FROM users WHERE username = ?", req.body.username);
-                    if (await bcrypt.compare(req.body.password, query[0][0].password))
+                    let usersQuery = await db.query("SELECT * FROM users WHERE email = ?", req.body.email);
+                    if (usersQuery[0].length > 0)
                     {
-                        await db.query("INSERT INTO tokens(userid, date, token) VALUES(?, ?, ?)", [query[0][0].id, new Date(), tokenGenerator(20)]);
+                        if (await bcrypt.compare(req.body.password, usersQuery[0][0].password))
+                        {
+                            let tokenQuery = await db.query("SELECT date, token FROM tokens WHERE userid = ?", usersQuery[0][0].id);
+                            if (isTokenValidBasedOnCurrentDateTime(tokenQuery[0][0].date, tokenExpiryInDays))
+                            {
+                                res.status(200);
+                                res.send(usersQuery[0][0]).end();
+                            }
+                            else
+                            {
+                                let token = tokenGenerator(20);
+                                await db.query("INSERT INTO tokens(userid, date, token) VALUES(?, ?, ?)", [usersQuery[0][0].id, new Date(), token]);
+                                res.status(200);
+                                res.send(usersQuery[0][0]).end();
+                            }
+                        }
+                        else
+                        {
+                            res.status(404);
+                            res.send({"message": "ERROR: The given email and password is not valid."}).end();
+                        }
                     }
-                    res.sendStatus(200).end();
+                    else
+                    {
+                        res.status(404);
+                        res.send({"message": "ERROR: The given email and password is not valid."}).end();
+                    }
                 }
                 catch (error)
                 {
                     res.status(503);
-                    res.send({"error": "Internal server error (Error message: " + error + ")"}).end();
+                    res.send({"message": "ERROR: " + error}).end();
                 }
             }
             else
             {
                 res.status(400);
-                res.send({"error": "Invalid data was given."}).end();
+                res.send({"message": "ERROR: Invalid data was given."}).end();
             }
         }
         else
         {
             res.status(400);
-            res.send({"error": "Values are not defined."}).end();
+            res.send({"message": "ERROR: Values are not defined."}).end();
         }
     }
     else
     {
         res.status(400);
-        res.send({"error": "The request's body is empty."}).end();
+        res.send({"message": "ERROR: The request's body is empty."}).end();
     }
 })
 
@@ -113,12 +138,7 @@ server.listen(5555, () => {
     console.log("Backend up! Avaiable at: localhost:5555")
 });
 
-function isNewTokenRequired(token, date)
-{
-
-}
-
-function isExpiredBasedOnCurrentDateTime(date, offset)
+function isTokenValidBasedOnCurrentDateTime(date, offset)
 {
     if (date.setDate(date.getDate() + offset) > new Date())
     {
