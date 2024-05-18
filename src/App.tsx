8da49071110364a9adf62a084f475a6173c8fba4
Benio-sama <vago.kata.viki@gmail.com>
2024-05-18 17:16:44 +0200

    email password login part works

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const API = "https://localhost:5555/"
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Login</h2>
+        <label htmlFor="email"> Email: </label><br />
+        <input id="email" type="email" placeholder="Email: "></input><br />
+        <label htmlFor="password"> Password: </label><br />
+        <input id="password" type="password" placeholder="Password: "></input><br />
+        <button onClick={async () => {
+          console.log('asd');
+          let email = (document.getElementById("email") as HTMLInputElement).value;
+          let password = (document.getElementById("password") as HTMLInputElement).value;
+
+          const data = {
+            email: email,
+            password: password
+          }
+          console.log(data);
+          const response = await fetch("https://localhost:5555/login", {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data),
+          });
+          const user = await response.json();
+          console.log(user);
+        }
+        }> Login </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
