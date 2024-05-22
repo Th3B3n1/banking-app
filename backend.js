3a29cb2e3676738c5aaf0bc62c026fb45bfe10cd
Th3B3n1 <othersc9bst@gmail.com>
2024-05-23 00:30:55 +0200

    /changePassword: thorrow testing; /register: done with thorrow testing; passwordComplianceChecker(): done; emailValidator(): done

--- backend.js
+++ backend.js
@@ -24,9 +24,11 @@ const db = mysql.createPool({
 }).promise();
 const jsonParser = bodyParser.json();
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const saltRounds = 10;
+/* TOKEN */
 const tokenExpiryInDays = 30;
-const minPasswordLength = 8;
+/* PASSWORD */
+const saltRounds = 10;
+/* ERRORMESSAGE */
 const errorMessages = Object.freeze(
 {
     ERR_INTERNAL_SERVER: "ERROR: Internal server error: ",
@@ -36,17 +38,22 @@ const errorMessages = Object.freeze(
     ERR_REQ_UNDEFINED_QUERY_VALUES: "ERROR: Some or all required Values are not defined in the request's query.",
     ERR_REQ_INVALID_BODY_VALUES: "ERROR: Invalid data was given to the request's body.",
     ERR_REQ_INVALID_QUERY_VALUES: "ERROR: Invalid data was given to the request's query.",
-    ERR_INVALID_CREDENTIALS: "ERROR: The given email and password are not in our database.",
-    ERR_INVALID_PASSWORD: "ERROR: The given password is incorrect based on our database records.",
-    ERR_NONCOMPLIANT_NEWPASSWORD: "ERROR: The given new password is not complying to our required standards.",
-    ERR_NO_USER_TO_GIVEN_TOKEN: "ERROR: No user is assigned to the given token.",
-    ERR_TOKEN_NOT_FOUND: "ERROR: Token not found. Please log in again.",
-    ERR_TOKEN_EXPIRED: "ERROR: Token expired. Please log in again."
-})
+    ERR_DATA_INVALID_CREDENTIALS: "ERROR: The given email and password are not in our database.",
+    ERR_DATA_INVALID_PASSWORD: "ERROR: The given password is incorrect based on our database records.",
+    ERR_DATA_NONCOMPLIANT_EMAIL: "ERROR: The given email is not a valid email.",
+    ERR_DATA_NONCOMPLIANT_PASSWORD: "ERROR: The given password is not complying to our required standards.",
+    ERR_DATA_NONCOMPLIANT_NEWPASSWORD: "ERROR: The given new password is not complying to our required standards.",
+    ERR_DATA_USER_ALREADY_EXISTS_EMAIL: "ERROR: A user already exists with the given email address.",
+    ERR_DATA_USER_ALREADY_EXISTS_FULLNAME: "ERROR: A user already exists with the given name.",
+    ERR_DATA_SAME_OLD_AND_NEW_PASSWORD: "ERROR: The old and new password has the same value.",
+    ERR_DATA_NO_USER_TO_GIVEN_TOKEN: "ERROR: No user is assigned to the given token.",
+    ERR_DATA_TOKEN_NOT_FOUND: "ERROR: Token not found. Please log in again.",
+    ERR_DATA_TOKEN_EXPIRED: "ERROR: Token expired. Please log in again."
+});
 const infoMessages = Object.freeze(
 {
 
-})
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
@@ -91,7 +98,7 @@ app.post("/login", jsonParser, async (req, res, next) =>
                             {
                                 usersQuery = await db.query("SELECT fullname, balance FROM users WHERE email = ?", req.body.email);
                                 res.status(200);
-                                res.send({token: tokenQuery[0][0], userData: usersQuery[0][0]})
+                                res.send({token: tokenQuery[0][0], userData: usersQuery[0][0]});
                                 res.end();
                             }
                             else
@@ -99,49 +106,49 @@ app.post("/login", jsonParser, async (req, res, next) =>
                                 let token = tokenGenerator(20);
                                 await db.query("INSERT INTO tokens(userid, date, token) VALUES(?, ?, ?)", [usersQuery[0][0].id, new Date(), token]);
                                 res.status(200);
-                                res.send({token: token, userData: usersQuery[0][0]})
+                                res.send({token: token, userData: usersQuery[0][0]});
                                 res.end();
                             }
                         }
                         else
                         {
                             res.status(404);
-                            res.send({message: errorMessages.ERR_INVALID_CREDENTIALS + " (debug: password)"})
+                            res.send({message: errorMessages.ERR_DATA_INVALID_CREDENTIALS + " (debug: password)"});
                             res.end();
                         }
                     }
                     else
                     {
                         res.status(404);
-                        res.send({message: errorMessages.ERR_INVALID_CREDENTIALS + " (debug: email)"})
+                        res.send({message: errorMessages.ERR_DATA_INVALID_CREDENTIALS + " (debug: email)"});
                         res.end();
                     }
                 }
                 catch (error)
                 {
                     res.status(503);
-                    res.send({message: errorMessages.ERR_INTERNAL_SERVER + error})
+                    res.send({message: errorMessages.ERR_INTERNAL_SERVER + error});
                     res.end();
                 }
             }
             else
             {
                 res.status(400);
-                res.send({message: errorMessages.ERR_REQ_INVALID_BODY_VALUES})
+                res.send({message: errorMessages.ERR_REQ_INVALID_BODY_VALUES});
                 res.end();
             }
         }
         else
         {
             res.status(400);
-            res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES})
+            res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES});
             res.end();
         }
     }
     else
     {
         res.status(400);
-        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY})
+        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY});
         res.end();
     }
 }, seamlessAuth, async (req, res) =>
@@ -153,20 +160,20 @@ app.post("/login", jsonParser, async (req, res, next) =>
         if (usersQuery[0].length > 0)
         {
             res.status(200);
-            res.send({token: undefined, userData: usersQuery[0][0]})
+            res.send({token: undefined, userData: usersQuery[0][0]});
             res.end();
         }
         else
         {
             res.status(404);
-            res.send({message: errorMessages.ERR_NO_USER_TO_GIVEN_TOKEN})
+            res.send({message: errorMessages.ERR_DATA_NO_USER_TO_GIVEN_TOKEN});
             res.end();
         }
     }
     catch (error)
     {
         res.status(503);
-        res.send({message: errorMessages.ERR_INTERNAL_SERVER + error})
+        res.send({message: errorMessages.ERR_INTERNAL_SERVER + error});
         res.end();
     }
 })
@@ -177,22 +184,76 @@ app.post("/register", jsonParser, async (req, res) =>
     {
         if ((req.body.email != undefined && req.body.password != undefined) && req.body.fullname != undefined)
         {
-            if ((emailValidator(req.body.email)))
+            try
             {
-                if (passwordComplianceChecker(req.body.password))
+                if ((emailValidator(req.body.email)))
                 {
-                    if (req.body.fullname.length > 0)
+                    if (passwordComplianceChecker(req.body.password))
                     {
-                        let hash = await bcrypt.hash(req.body.password, saltRounds)
+                        if (req.body.fullname.length > 0)
+                        {
+                            let usersQuery = await db.query("SELECT email FROM users WHERE email = ?", req.body.email);
+                            if (usersQuery[0].length == 0)
+                            {
+                                usersQuery = await db.query("SELECT fullname FROM users WHERE fullname = ?", req.body.fullname);
+                                if (usersQuery[0].length == 0)
+                                {
+                                    let hash = await bcrypt.hash(req.body.password.trim(), saltRounds);
+                                    await db.query("INSERT INTO users(email, password, fullname) VALUES(?, ?, ?)", [req.body.email, hash, req.body.fullname]);
+                                }
+                                else
+                                {
+                                    res.status(400);
+                                    res.send({message: errorMessages.ERR_DATA_USER_ALREADY_EXISTS_FULLNAME});
+                                    res.end();
+                                }
+                            }
+                            else
+                            {
+                                res.status(400);
+                                res.send({message: errorMessages.ERR_DATA_USER_ALREADY_EXISTS_EMAIL});
+                                res.end();
+                            }
+                        }
+                        else
+                        {
+                            res.status(400);
+                            res.send({message: errorMessages.ERR_REQ_INVALID_BODY_VALUES});
+                            res.end();
+                        }
+                    }
+                    else
+                    {
+                        res.status(400);
+                        res.send({message: errorMessages.ERR_DATA_NONCOMPLIANT_PASSWORD});
+                        res.end();
                     }
                 }
+                else
+                {
+                    res.status(400);
+                    res.send({message: errorMessages.ERR_DATA_NONCOMPLIANT_EMAIL});
+                    res.end();
+                }
             }
+            catch (error)
+            {
+                res.status(503);
+                res.send({message: errorMessages.ERR_INTERNAL_SERVER + error});
+                res.end();
+            }
+        }
+        else
+        {
+            res.status(400);
+            res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES});
+            res.end();
         }
     }
     else
     {
         res.status(400);
-        res.send({"message": errorMessages.ERR_REQ_UNDEFINED_BODY})
+        res.send({"message": errorMessages.ERR_REQ_UNDEFINED_BODY});
         res.end();
     }
 })
@@ -204,57 +265,66 @@ app.post("/changePassword", jsonParser, seamlessAuth, async (req, res) =>
     {
         if (req.body.oldPassword.length > 0 && req.body.newPassword.length > 0)
         {
-            try
+            if (req.body.oldPassword != req.body.newPassword)
             {
-                let usersQuery = await db.query("SELECT password FROM users WHERE id = ?", userId);
-                if (usersQuery[0].length > 0)
+                try
                 {
-                    if (await bcrypt.compare(req.body.oldPassword, usersQuery[0][0].password))
+                    let usersQuery = await db.query("SELECT password FROM users WHERE id = ?", userId);
+                    if (usersQuery[0].length > 0)
                     {
-                        if (passwordComplianceChecker(req.body.newPassword))
+                        if (await bcrypt.compare(req.body.oldPassword, usersQuery[0][0].password))
                         {
-                            let hash = await bcrypt.hash(req.body.newPassword, saltRounds)
-                            await db.query("UPDATE users SET password = ? WHERE id = ?", [hash, userId])
+                            if (passwordComplianceChecker(req.body.newPassword))
+                            {
+                                let hash = await bcrypt.hash(req.body.newPassword.trim(), saltRounds);
+                                await db.query("UPDATE users SET password = ? WHERE id = ?", [hash, userId]);
+                            }
+                            else
+                            {
+                                res.status(400);
+                                res.send({message: errorMessages.ERR_DATA_NONCOMPLIANT_NEWPASSWORD});
+                                res.end();
+                            }
                         }
                         else
                         {
                             res.status(400);
-                            res.send({message: errorMessages.ERR_NONCOMPLIANT_NEWPASSWORD})
+                            res.send({message: errorMessages.ERR_DATA_INVALID_PASSWORD});
                             res.end();
                         }
                     }
                     else
                     {
-                        res.status(400);
-                        res.send({message: errorMessages.ERR_INVALID_PASSWORD})
+                        res.status(404);
+                        res.send({message: errorMessages.ERR_DATA_NO_USER_TO_GIVEN_TOKEN});
                         res.end();
                     }
                 }
-                else
+                catch (error)
                 {
-                    res.status(404);
-                    res.send({message: errorMessages.ERR_NO_USER_TO_GIVEN_TOKEN})
+                    res.status(503);
+                    res.send({message: errorMessages.ERR_INTERNAL_SERVER + error});
                     res.end();
                 }
             }
-            catch (error)
+            else
             {
-                res.status(503);
-                res.send({message: errorMessages.ERR_INTERNAL_SERVER + error})
+                res.status(400);
+                res.send({message: errorMessages.ERR_DATA_SAME_OLD_AND_NEW_PASSWORD});
                 res.end();
             }
         }
         else
         {
             res.status(400);
-            res.send({message: errorMessages.ERR_REQ_INVALID_BODY_VALUES})
+            res.send({message: errorMessages.ERR_REQ_INVALID_BODY_VALUES});
             res.end();
         }
     }
     else
     {
         res.status(400);
-        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES})
+        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES});
         res.end();
     }
 })
@@ -289,57 +359,56 @@ async function seamlessAuth(req, res, next)
                     }
                     else
                     {
-                        res.send({message: errorMessages.ERR_TOKEN_EXPIRED})
+                        res.send({message: errorMessages.ERR_DATA_TOKEN_EXPIRED});
                         res.end();
                     }
                 }
                 else
                 {
                     res.status(404);
-                    res.send({message: errorMessages.ERR_TOKEN_NOT_FOUND})
+                    res.send({message: errorMessages.ERR_DATA_TOKEN_NOT_FOUND});
                     res.end();
                 }
             }
             catch (error)
             {
                 res.status(503);
-                res.send({message: errorMessages.ERR_INTERNAL_SERVER + error})
+                res.send({message: errorMessages.ERR_INTERNAL_SERVER + error});
                 res.end();
             }
         }
         else
         {
             res.status(400);
-            res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES})
+            res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY_VALUES});
             res.end();
         }
     }
     else
     {
         res.status(400);
-        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY})
+        res.send({message: errorMessages.ERR_REQ_UNDEFINED_BODY});
         res.end();
     }
 }
 
 function emailValidator(emailAddress)
 {
-    if (emailAddress)
+    if (emailAddress.length > 0)
     {
-
-    }
-}
-
-function passwordComplianceChecker(password)
-{
-    if (password.length > minPasswordLength)
-    {
-        
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(emailAddress);
     }
     else
     {
         return false;
     }
+}
+
+function passwordComplianceChecker(password)
+{
+    const regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return regex.test(password);
 }
 
 function isTokenValidBasedOnCurrentDateTime(date, offset)
